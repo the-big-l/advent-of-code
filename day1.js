8318afc0e9e04d30a65d9c3e5d79c541331ed25a
Leo Salat <leo.salat@gmail.com>
2026-04-05 14:49:17 -0700

    refactor part 1; add semantic naming; add comments

--- day1.js
+++ day1.js
@@ -1,47 +1,46 @@
 // read from file named arguement 1
 const fileName = process.argv[2];
-
 const fs = require('fs');
-
 const directions = fs.readFileSync(fileName, 'utf-8').split('\n');
 
 const size = 100;
-let pos = 50;
+let position = 50;
 let counter = 0;
 
 directions.forEach((x) => {
-	const dir = x[0]
-	const num = +x.slice(1)
+	const direction = x[0] == 'L' ? -1 : 1
+	const clicks = +x.slice(1) * direction
 
-	if (dir == 'L') {
-		pos = pos - num
-	} else {
-		pos = pos + num
-	}
+	position = (position + clicks % size + size) % size
 
-	if (pos % size == 0) {
+	if (position % size == 0) {
 		counter++
 	}
 })
 
 console.log("the password for part 1 is:", counter) 
 
-pos = 50;
+position = 50;
 counter = 0;
 
 for (x of directions) {
-	const dir = x[0] == 'L' ? -1 : 1
-	const mag = +x.slice(1)
-	const clicks = dir * mag
-	const net = clicks % size
+	const direction = x[0] == 'L' ? -1 : 1
+	const magnitude = +x.slice(1)
+	const totalClicks = direction * magnitude
+	const netClicks = totalClicks % size
 
-	counter += Math.floor(Math.abs(clicks) / size)
+	// add full rotations to counter
+	counter += Math.floor(Math.abs(totalClicks) / size)
 	
-	if (pos != 0 && pos + net <= 0 || pos + net >= size) {
+	// increment counter if position will end on 0 OR goes out of bounds
+	// but ONLY if it didn't already start on 0
+	if (position != 0 && position + netClicks <= 0 || position + netClicks >= size) {
 		counter++
 	}
 	
-	pos = (pos + net % size + size) % size
+	// update dial position
+	// copied from array bi-directional, wrap-around implementation
+	position = (position + netClicks % size + size) % size
 }
 
-console.log("this is the password for part 2: ", counter)
+console.log("this is the password for part 2:", counter)
